28ce11dff1d86aa0cba9be36109dcf9466733ac1
Yousef <yousef.r.haggy@gmail.com>
2020-02-02 14:22:41 -0500

    Flaskifyed

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import spacy
 import requests
+from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
 
+app = Flask(__name__)
+
 SMMRY_API_KEY = "92E708A8B3"
+
 def get_transcript(video_id, percent):
 	transcript = {}
 	try:
@@ -26,11 +30,18 @@ def identify_sentences(transcript_string, percent):
 		num_sentences+=1
 	return get_summary(transcript_with_sentences, percent, num_sentences)
 def get_summary(transcript, percent, num_sentences):
-	summary_length= round(percent*num_sentences)
+	summary_length= round(float(percent)*num_sentences)
 	api_url= ("http://api.smmry.com/&SM_API_KEY=%s&SM_LENGTH=%s" 
 			% (SMMRY_API_KEY,summary_length))
 	r = requests.post(api_url, data={"sm_api.input":transcript})
 	return r.json()["sm_api_content"]
-print(get_transcript("J12TTFu0mss",.1))
+
+@app.route("/")
+def summarize():
+	video_id = request.args.get("video_id")
+	percent_to_summarize= request.args.get("percent")
+	return get_transcript(video_id, percent_to_summarize)
+
+
 
 
